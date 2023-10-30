197c4083558de1ea343347061540909b074f3c49
cristhianlgl <cristhianlgl@hotmail.com>
2023-10-30 17:10:32 -0500

    agregar seccion de testimonios

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -35,8 +35,8 @@ export default {
 				service : 'url(../assets/img/services/bg.svg)',
 				testimonials : 'url(../assets/img/testimonials/bg.svg)',
 				departments: 'url(../assets/img/departments/bg.svg)',
-				quoteLeft: 'url(../assets/img/testimonials/quote-left.svg)',
-				quoteRight: 'url(../assets/img/testimonials/quote-right.svg)',
+				quoteLeft: 'url(../assets/icons/testimonials/quote-left.svg)',
+				quoteRight: 'url(../assets/icons/testimonials/quote-right.svg)',
 			}
 		},
 	},

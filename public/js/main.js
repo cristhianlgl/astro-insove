68a2505e36080077ec0212583d70e0bda9f39d5c
cristhianlgl <cristhianlgl@hotmail.com>
2023-10-31 11:47:40 -0500

    agregar seccion team

--- public/js/main.js
+++ public/js/main.js
@@ -14,3 +14,20 @@ const swiper = new Swiper('#swiper', {
       clickable: true,
   },
 });
+
+const swiperTeam = new Swiper('#swiper_team', {
+  slidesPerView: 1,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+  },
+});
